f23cd30558a4825f55e6606bd20e62732b97d6d4
EvertonFrankllin <evertonfrankllintrc@gmail.com>
2024-06-17 12:12:50 -0300

    Responsividade

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Aboult />
       <Home />
+      <Aboult />
       <Carreira />
       <School />
       <Project />
